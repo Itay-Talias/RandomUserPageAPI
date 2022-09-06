98d6ea8c541c3ad3245cb581a6e650bfea71b0ce
Itay.Talias <itay.talias@gmail.com>
2022-09-06 18:17:56 +0300

    extenstions-init popup

--- dest/main.js
+++ dest/main.js
@@ -1,3 +1,8 @@
 "use strict";
 const userPageApi = new ControllerModule();
 userPageApi.addOnClicksToButtons();
+function myFunction() {
+    console.log("hii");
+    const popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 const userPageApi = new ControllerModule();
 
 userPageApi.addOnClicksToButtons();
+
+function myFunction() {
+    console.log("hii");
+    const popup = document.getElementById("myPopup") as HTMLInputElement;
+    popup.classList.toggle("show");
+}
